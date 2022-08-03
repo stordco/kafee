baea66722856a150bc564f7e9b9a776d1f73cbe5
Blake Kostner <git@btkostner.io>
2022-08-03 13:19:34 -0600

    chore: remove application supervisor

--- mix.exs
+++ mix.exs
@@ -16,14 +16,16 @@ defmodule Bauer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Bauer.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:elsa, "~> 1.0.0-rc.3"},
+
+      # Dev & Test dependencies
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
