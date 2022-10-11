6a08d5d6a1b7cc1cb40794816b7b4ca097f26fa2
Blake Kostner <git@btkostner.io>
2022-10-11 17:19:23 -0600

    fix tests

--- lib/kafee/producer/config.ex
+++ lib/kafee/producer/config.ex
@@ -217,10 +217,10 @@ defmodule Kafee.Producer.Config do
   ## Examples
 
       iex> brod_client_config(%Config{ssl: false})
-      [auto_start_producers: true, ssl: false]
+      [request_timeout: 5000, connect_timeout: 5000, auto_start_producers: true, ssl: false]
 
       iex> brod_client_config(%Config{ssl: false, brod_client_opts: [query_api_version: false]})
-      [auto_start_producers: true, ssl: false, query_api_version: false]
+      [request_timeout: 5000, connect_timeout: 5000, auto_start_producers: true, ssl: false, query_api_version: false]
 
   """
   @spec brod_client_config(t()) :: :brod.client_config()
