27acd214225f6af1b2100ebfc78e0f3faa28ec36
Blake Kostner <git@btkostner.io>
2023-01-31 14:41:22 -0700

    fix: update telemetry error kind to struct name

--- lib/kafee/producer/async_worker.ex
+++ lib/kafee/producer/async_worker.ex
@@ -156,7 +156,7 @@ defmodule Kafee.Producer.AsyncWorker do
     rescue
       err ->
         emit_produce_end_telemetry(state, :exception, %{
-          kind: err.kind,
+          kind: err.__struct__,
           reason: :exception,
           stacktrace: __STACKTRACE__
         })
