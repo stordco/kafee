2352127c8d1c6911a4d4f63d01011a6e53a2f34f
Seungjin Kim <seungjin.kim@stord.com>
2024-09-26 14:40:42 -0700

    ADD: Broadway test

--- test/kafee/consumer/broadway_adapter_integration_test.exs
+++ test/kafee/consumer/broadway_adapter_integration_test.exs
@@ -11,11 +11,40 @@ defmodule Kafee.Consumer.BroadwayAdapterIntegrationTest do
     end
   end
 
+  defmodule MyConsumerBatched do
+    use Kafee.Consumer,
+      adapter:
+        {Kafee.Consumer.BroadwayAdapter,
+         [
+           batching: [
+             concurrency: 2,
+             size: 5,
+             timeout: 10,
+             async_run: true
+           ]
+         ]}
+
+    def handle_message(%Kafee.Consumer.Message{} = message) do
+      test_pid = Application.fetch_env!(:kafee, :test_pid)
+      send(test_pid, {:consume_message, message, self()})
+    end
+  end
+
   setup %{topic: topic} do
     Application.put_env(:kafee, :test_pid, self())
 
     start_supervised!(
       {MyConsumer,
+       [
+         host: KafkaApi.host(),
+         port: KafkaApi.port(),
+         topic: topic,
+         consumer_group_id: KafkaApi.generate_consumer_group_id()
+       ]}
+    )
+
+    start_supervised!(
+      {MyConsumerBatched,
        [
          host: KafkaApi.host(),
          port: KafkaApi.port(),
@@ -38,5 +67,23 @@ defmodule Kafee.Consumer.BroadwayAdapterIntegrationTest do
       key = "key-#{i}"
       assert_receive {:consume_message, %Kafee.Consumer.Message{key: ^key}}
     end
+  end
+
+  # credo:disable-for-lines:2 Credo.Check.Readability.StrictModuleLayout
+  @tag topic: "my-test-topic-for-batch"
+  test "it processes messages asynchronously in batches", %{brod_client_id: brod, topic: topic} do
+    for i <- 1..100 do
+      :ok = :brod.produce_sync(brod, topic, :hash, "key-#{i}", "test value")
+    end
+
+    task_pids =
+      for i <- 1..100 do
+        key = "key-#{i}"
+        assert_receive {:consume_message, %Kafee.Consumer.Message{key: ^key}, from_pid}
+        from_pid
+      end
+
+    # assert they were done asynchronously
+    assert 100 == task_pids |> Enum.uniq() |> length
   end
 end
