85c090151731ef5b68b192fb8ba44b54a8742e33
Blake Kostner <git@btkostner.io>
2024-11-11 20:35:24 -0800

    fix: update kafee consumer handle_failure/2 fn

--- lib/kafee/consumer.ex
+++ lib/kafee/consumer.ex
@@ -192,6 +192,8 @@ defmodule Kafee.Consumer do
   @doc false
   defmacro __using__(opts \\ []) do
     quote location: :keep, bind_quoted: [opts: opts, module: __CALLER__.module] do
+      require Logger
+
       @behaviour Kafee.Consumer
 
       @doc false
@@ -230,8 +232,7 @@ defmodule Kafee.Consumer do
         inspected_message = inspect(message)
         inspected_error = inspect(error)
 
-        raise RuntimeError,
-          message: """
+        Logger.error("""
           An error has been raised while processing a Kafka message.
 
           Message:
@@ -239,7 +240,7 @@ defmodule Kafee.Consumer do
 
           Error:
           #{inspected_error}
-          """
+        """, kafee_message: message, error: error)
       end
 
       defoverridable child_spec: 1, handle_message: 1, handle_failure: 2
