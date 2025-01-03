e234a99ce655ab4ec67b91c093b72546119f4b3a
Seungjin Kim <seungjin.kim@stord.com>
2025-01-02 17:51:04 -0800

    UPDATE: lag asseerting test correctly asserts

--- test/kafee/consumer/brod_monitor_test.exs
+++ test/kafee/consumer/brod_monitor_test.exs
@@ -1,6 +1,6 @@
 defmodule Kafee.Consumer.BrodMonitorTest do
   use Kafee.KafkaCase
-  alias Kafee.Consumer.{Adapter, BrodMonitor}
+  alias Kafee.Consumer.BrodMonitor
 
   defmodule MyProducer do
     use Kafee.Producer,
@@ -9,6 +9,17 @@ defmodule Kafee.Consumer.BrodMonitorTest do
 
     def publish(_type, messages) do
       :ok = produce(messages)
+    end
+  end
+
+  defmodule MyLaggyConsumer do
+    use Kafee.Consumer,
+      adapter: {Kafee.Consumer.BroadwayAdapter, []}
+
+    def handle_message(%Kafee.Consumer.Message{} = message) do
+      test_pid = Application.get_env(:kafee, :test_pid, self())
+      Process.sleep(100)
+      send(test_pid, {:consume_message, message})
     end
   end
 
@@ -22,33 +33,34 @@ defmodule Kafee.Consumer.BrodMonitorTest do
        ]}
     )
 
-    :ok
+    [topic: topic]
   end
 
-  setup(%{topic: topic}) do
-    consumer_group_id = Kafee.KafkaApi.generate_consumer_group_id()
+  setup %{topic: topic} do
+    consumer_group_id = KafkaApi.generate_consumer_group_id()
 
-    consumer_pid =
-      start_supervised!(%{
-        id: MyConsumer,
-        start:
-          {Kafee.Consumer, :start_link,
-           [
-             MyConsumer,
-             [
-               adapter: Kafee.Consumer.BroadwayAdapter,
-               host: Kafee.KafkaApi.host(),
-               port: Kafee.KafkaApi.port(),
-               consumer_group_id: consumer_group_id,
-               topic: topic
-             ]
-           ]}
-      })
-
-    [consumer_pid: consumer_pid, consumer_group_id: consumer_group_id]
+    [consumer_group_id: consumer_group_id]
   end
 
   describe "get_consumer_lag/5" do
+    setup %{topic: topic, consumer_group_id: consumer_group_id} do
+      Application.put_env(:kafee, :test_pid, self())
+
+      consumer_pid =
+        start_supervised!(
+          {MyLaggyConsumer,
+           [
+             host: KafkaApi.host(),
+             port: KafkaApi.port(),
+             topic: topic,
+             consumer_group_id: consumer_group_id
+           ]}
+        )
+
+      Process.sleep(10_000)
+      [consumer_pid: consumer_pid]
+    end
+
     test "should correctly handle zero consumed case", %{
       brod_client_id: brod_client_id,
       topic: topic,
@@ -66,10 +78,35 @@ defmodule Kafee.Consumer.BrodMonitorTest do
                ])
 
       partitions_list = Enum.map(0..(partitions - 1), & &1)
-      poll_until_offset_tick(topic, partitions_list, [{0, 1}])
+      assert :ok = poll_until_offset_tick(topic, partitions_list, [{0, 1}])
 
       assert {:ok, %{}} =
                BrodMonitor.get_consumer_lag(brod_client_id, Kafee.BrodApi.endpoints(), topic, consumer_group_id)
+    end
+
+    test "should correctly return consumer lag", %{
+      brod_client_id: brod_client_id,
+      topic: topic,
+      partitions: partitions,
+      consumer_group_id: consumer_group_id
+    } do
+      for i <- 1..100 do
+        :ok = :brod.produce_sync(brod_client_id, topic, :hash, "key-#{i}", "test value")
+      end
+
+      assert_receive {:consume_message, _}
+
+      partitions_list = Enum.map(0..(partitions - 1), & &1)
+      assert :ok = poll_until_offset_tick(topic, partitions_list, [{0, 100}])
+
+      # wait a bit for consumer lag to build up
+      Process.sleep(100)
+
+      # since LaggyConsumer takes some time to process each message, we'll see lags
+      assert {:ok, %{0 => lag}} =
+               BrodMonitor.get_consumer_lag(brod_client_id, Kafee.BrodApi.endpoints(), topic, consumer_group_id)
+
+      assert lag > 20
     end
   end
 
